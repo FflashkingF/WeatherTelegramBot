2f8aa6dda6e6a4462c599919d653b2694e54caa3
Фёдор Асташкин <astashkin.fk@phystech.edu>
2023-05-04 23:54:09 +0300

    some changes

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # WeatherTelegramBot
+
+git clone git@github.com:FflashkingF/WeatherTelegramBot.git   <br />
+cd WeatherTelegramBot  <br />
+git branch checkout dev   <br />
+pip install -r requirements.txt   <br />
+you must create config_.py and write constants in this file: <br />
+  BOT_API = ... (your telegram bot API) <br />
+  WEATHER_API = ... (OpenWeather API, you can take it after registration https://home.openweathermap.org/users/sign_up) <br />
+python3 bot.py   <br />
--- bot.py
+++ bot.py
@@ -2,9 +2,11 @@ import logging
 import requests
 import config_
 import keyboards_
+import json
 from pprint import pprint
 from aiogram import Bot, Dispatcher, types, executor
 from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher.filters import Text
 from aiogram.dispatcher import FSMContext
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
@@ -16,14 +18,18 @@ storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 
 
-@dp.message_handler(commands=['start', 'help'])
+@dp.message_handler(state='*', commands=['stop'])
 async def send_welcome(message: types.Message):
-    await message.reply('Hello', reply_markup=keyboards_.start_kb)
+    await message.reply('Goodbye', reply_markup=keyboards_.ReplyKeyboardRemove())
 
 
-@dp.message_handler(commands=['stop'])
-async def send_welcome(message: types.Message):
-    await message.reply('Goodbye')
+@dp.message_handler(state='*', commands='cancel')
+@dp.message_handler(Text(equals='cancel', ignore_case=True), state='*')
+async def cancel_handler(message: types.Message, state: FSMContext):
+    current_state = await state.get_state()
+    if current_state is not None:
+        await state.finish()
+    await message.reply('Cancelled.', reply_markup=keyboards_.start_kb)
 
 
 class Input_location(StatesGroup):
@@ -49,13 +55,13 @@ async def try_answer(message: types.Message, url: str, location: str, state: FSM
 
     print(response.status_code)
     if response.status_code == 200:
-        #print(response)
+        # print(response)
         weather_data = response.json()
-        #print(weather_data)
+        # print(weather_data)
         pprint(weather_data)
         temperature = weather_data['main']['temp']
         temperature_feel = weather_data['main']['feels_like']
-        description : str = weather_data['weather'][0]['description']
+        description: str = weather_data['weather'][0]['description']
         wind_speed = weather_data['wind']['speed']
         location = weather_data['name']
         country = weather_data['sys']['country']
@@ -63,10 +69,8 @@ async def try_answer(message: types.Message, url: str, location: str, state: FSM
             f'It is feels like {temperature_feel:0.1f}°C\n' + \
             f'{description.capitalize()}\n' + \
             f'Wind speed is {wind_speed:0.1f}m/s'
-        success = True
         await state.finish()
     else:
-        success = False
         message_text = f"Sorry, I couldn't find weather information for {location}.\n" + \
             "Please try again."
     await message.answer(message_text,  reply_markup=keyboards_.start_kb)
@@ -92,6 +96,16 @@ async def process_input_location(message: types.Message, state: FSMContext):
 
     await try_answer(message, url, location, state)
 
+
+@dp.message_handler()  # anything or help, start
+async def help_handler(message: types.Message):
+    message_text = 'Instruction\n' + \
+        '/Write_location to ask weather in location which you want\n' + \
+        f'Press button "{json.loads(keyboards_.button_location.as_json())["text"]}" to see weather ' +  \
+        'in your own location(you also can drop custom location)\n' + \
+        '/cancel to return in /start' + \
+        '/stop to stop bot'
+    await message.reply(message_text, reply_markup=keyboards_.start_kb)
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
